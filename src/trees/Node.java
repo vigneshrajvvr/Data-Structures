2d43788b09013c244decbe6d580a00b40310d6de
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2020-07-30 22:04:22 +0530

    Node class

--- src/trees/Node.java
+++ src/trees/Node.java
@@ -1,5 +1,41 @@
 package trees;
 
 public class Node {
+	
+	private int value;
+	private Node left;
+	private Node right;
 
+    public Node(int value, Node left, Node right) {
+	    this.value = value;
+	    this.left = left;
+	    this.right = right;
+	}
+
+	public Node (int value) {
+	    this.value = value;
+	    this.left = null;
+	    this.right = null;
+	}
+
+	public Node getLeft() {
+	    return left;
+	}
+
+	public void setLeft(Node left) {
+	    this.left = left;
+	 }
+
+	public Node getRight() {
+	    return right;
+	}
+
+	public void setRight(Node right) {
+	    this.right = right;
+	}
+
+	public int getValue() {
+	    return value;
+	}
 }
+	
