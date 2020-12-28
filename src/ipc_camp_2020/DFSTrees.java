bd5ccac0053fc58b558a12c806e7c0ff3cc6f811
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2020-12-28 13:14:06 +0530

    DFS in trees

--- src/ipc_camp_2020/DFSTrees.java
+++ src/ipc_camp_2020/DFSTrees.java
@@ -5,16 +5,20 @@ import java.util.List;
 import java.util.Scanner;
 
 public class DFSTrees {
-	
 	private static List<Integer> nodes[];
-	
+	private static boolean[] visited;
 	public static void main(String args[]) {
-		
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter the number of nodes");
 		int n = in.nextInt();
-		nodes = new ArrayList[n];
-			
+		nodes = new ArrayList[n+1];
+		visited = new boolean[n+1];
+		
+		for(int i=0;i<nodes.length;i++) {
+			nodes[i] = new ArrayList<>();
+		}
+		
+		System.out.println("Enter the edges : ");
 		for(int i=0;i<n-1;i++) {
 			int u = in.nextInt();
 			int v = in.nextInt();
@@ -22,6 +26,23 @@ public class DFSTrees {
 			nodes[v].add(u);
 		}
 		
+		dfs();
+		
 	}
-
+	
+	public static void dfs() {
+		dfs(1);
+	}
+	
+	public static void dfs(int startNode) {
+		if(visited[startNode]) {
+			return;
+		}
+		System.out.println(startNode);
+		visited[startNode] = true;
+		List<Integer> tempList = nodes[startNode];
+		for(int i=0;i<tempList.size();i++) {
+			dfs(tempList.get(i));
+		}
+	}
 }
