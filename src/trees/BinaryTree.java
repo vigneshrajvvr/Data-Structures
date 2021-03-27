5db5d4cdc18e63ee41e4781e4317b55deb56be8d
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-03-27 10:05:42 +0530

    Binary trees

--- src/trees/BinaryTree.java
+++ src/trees/BinaryTree.java
@@ -1,34 +1,42 @@
 package trees;
 
-public class BinaryTree {
+class Node {
 	
-	class Node {
-		private Node left;
-		private Node right;
-		private int value;
-		
-		public Node getLeft() {
-			return left;
-		}
-		public void setLeft(Node left) {
-			this.left = left;
-		}
-		public Node getRight() {
-			return right;
-		}
-		public void setRight(Node right) {
-			this.right = right;
-		}
-		public int getValue() {
-			return value;
-		}
-		public void setValue(int value) {
-			this.value = value;
-		}
+	private Node left;
+	private Node right;
+	private int value;
+	
+	public Node(int value) {
+		this.value = value;
 	}
 	
+	public Node getLeft() {
+		return left;
+	}
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+	public Node getRight() {
+		return right;
+	}
+	public void setRight(Node right) {
+		this.right = right;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
+
+public class BinaryTree {
+	
 	public static void main(String args[]) {
-		
+		Node root = new Node(10);
+		root.setLeft(new Node(20));
+		root.setRight(new Node(30));
+		root.getLeft().setRight(new Node(40));
 	}
 
 }
