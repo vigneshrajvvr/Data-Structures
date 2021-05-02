3d77a9e186a2e9892164298cbdbb975743fdb671
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-05-02 23:33:07 +0530

    Prime factors program

---
+++ src/mathematics/PrimeFactors.java
@@ -0,0 +1,40 @@
+package mathematics;
+
+public class PrimeFactors {
+	
+	public static void primeFactorOptimized(int n) {
+		
+	}
+	
+	public static void primeFactorsBruteForce(int n) {
+		for(int i = 2; i< n;i++) {
+			if(prime(i)) {
+				int x = i;
+				while(n % x == 0) {
+					System.out.println(i);
+					x = x * i;
+				}
+			}
+		}
+	}
+	
+	private static boolean prime(int n) {
+		if(n == 2 || n == 3) {
+			return true;
+		}
+		if(n % 2 == 0 || n % 3 == 0) {
+			return false;
+		}
+		for(int i = 5; i * i <=n;i+=6) {
+			if(n % 5 == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String args[]) {
+		primeFactorsBruteForce(12);
+	}
+
+}
