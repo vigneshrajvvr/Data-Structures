a1da77b2df0c30d0c569037b6587b9cb3127b72e
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-05-03 00:02:43 +0530

    Efficient solution

--- src/mathematics/PrimeFactors.java
+++ src/mathematics/PrimeFactors.java
@@ -2,11 +2,26 @@ package mathematics;
 
 public class PrimeFactors {
 	
+	
+	// n^1/2 * logn
 	public static void primeFactorOptimized(int n) {
-		
+		if(n < 1) {
+			return;
+		}
+		System.out.println("Optimized: ");
+		for(int i = 2; i*i <= n ; i++) {
+			while(n % i == 0) {
+				System.out.println(i);
+				n = n/i;
+			}
+		}
 	}
 	
+	// n * n ^ (1/2) * logn
 	public static void primeFactorsBruteForce(int n) {
+		if(n < 1) {
+			return;
+		}
 		for(int i = 2; i< n;i++) {
 			if(prime(i)) {
 				int x = i;
@@ -34,7 +49,9 @@ public class PrimeFactors {
 	}
 	
 	public static void main(String args[]) {
-		primeFactorsBruteForce(12);
+//		primeFactorsBruteForce(12);
+//		primeFactorOptimized(12);
+		primeFactorOptimized(150000);
 	}
 
 }
