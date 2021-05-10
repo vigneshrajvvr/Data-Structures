f26d00b3336a643aea3d8b5e743add7aa0a3d7a7
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-05-10 21:36:06 +0530

    Power function

--- src/mathematics/Power.java
+++ src/mathematics/Power.java
@@ -3,6 +3,7 @@ package mathematics;
 public class Power {
 	
 	// Time complexity: O(n)
+	// Space complexity: O(1)
 	public static int powerBruteForce(int x, int n) {
 		int result = 1;
 		for(int i = 1;i<=n;i++) {
@@ -11,6 +12,9 @@ public class Power {
 		return result;
 	} 
 	
+	// Time complexity: O(log n)
+	// Space complexity: O(log n)
+	// Recursive solution
 	public static int powerOptimised(int x, int n) {
 		if(n == 0) {
 			return 1;
@@ -25,13 +29,43 @@ public class Power {
 		}
 	}
 	
+	// Time complexity: O(log n)
+	// Space complexity: O(1)
+	// Binary Exponentiation
+	public static int binaryExponentiation(int x, int n) {
+		int result = 1;
+		while(n > 0) {
+			if(n % 2 != 0) {
+				result *= x;
+				System.out.print(1 + " " );
+			} else {
+				System.out.print(0 + " ");
+			}
+			n = n/2;
+			x = x * x;
+		}
+		return result;
+	}
+	 
 	public static void main(String args[]) {
+		// Brute force
+		System.out.println("Brute force solution : ");
 		System.out.println(powerBruteForce(2,3));
 		System.out.println(powerBruteForce(3,4));
 		System.out.println(powerBruteForce(5,0));
 		System.out.println(powerBruteForce(5,1));
 		
+		// Recursive solution
+		System.out.println("Recursive solution : ");
 		System.out.println(powerOptimised(2,6));
-
+		
+		// Iterative solution
+		System.out.println("Binary exponentiation : ");
+		System.out.println(binaryExponentiation(2,3));
+		System.out.println(binaryExponentiation(3,4));
+		System.out.println(binaryExponentiation(5,0));
+		System.out.println(binaryExponentiation(5,1));
+		System.out.println(binaryExponentiation(4,5));
+		
 	}
 }
