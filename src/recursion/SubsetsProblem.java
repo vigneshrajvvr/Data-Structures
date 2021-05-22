c2f307bf1be7ca0127abec7e0ae882951e7898c1
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-05-22 23:00:46 +0530

    Subset of string and array

--- src/recursion/SubsetsProblem.java
+++ src/recursion/SubsetsProblem.java
@@ -1,5 +1,8 @@
 package recursion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubsetsProblem {
 	
 	
@@ -14,8 +17,20 @@ public class SubsetsProblem {
 		subset(s, current + Character.toString(s.charAt(i)), i+1);
 	}
 	
+	public static void subsetMethod1(char[] arr, List<String> tempList, int start) {
+		System.out.println(tempList.toString());
+		for(int i=start; i < arr.length; i++) {
+			tempList.add(Character.toString(arr[i]));
+			subsetMethod1(arr, tempList, i + 1);
+			tempList.remove(tempList.size() - 1);
+		}
+		
+		
+	}
+	
 	public static void main(String args[]) {
-		subset("ABC","", 0);
+//		subset("ABC","", 0);
+		subsetMethod1("ABC".toCharArray(), new ArrayList<>(), 0);
 	}
 	
 }
