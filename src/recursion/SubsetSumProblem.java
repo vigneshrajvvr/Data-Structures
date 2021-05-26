be07cca081aabc0fce11b9f4a3788a0d08c00fb4
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-05-26 16:17:41 +0530

    Another approach for subset sum problem

--- src/recursion/SubsetSumProblem.java
+++ src/recursion/SubsetSumProblem.java
@@ -28,12 +28,29 @@ public class SubsetSumProblem {
 		}
 	}
 	
+	public static int subsetApproach1(int arr[], int sum, int start) {
+		if(sum == 0) {
+			return 1;
+		}
+		
+		if(start >= arr.length) {
+			return 0;
+		}
+		
+		return subsetApproach1(arr, sum, start + 1) + subsetApproach1(arr, sum - arr[start], start + 1);		
+	}
+	
 	public static void main(String args[]) {
-		ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-		System.out.println(subsetSum(new int[]{10,5,2,3,6},8));
-		System.out.println(subsetSum(new int[] {10, 20, 15}, 0));
-		System.out.println(subsetSum(new int[] {1, 2, 3}, 4));
-		System.out.println(subsetSum(new int[] {10, 20, 15}, 37));
+		System.out.println(subsetSum(new int[]{10,5,2,3,6},8));    //2
+		System.out.println(subsetSum(new int[] {10, 20, 15}, 0));  //1
+		System.out.println(subsetSum(new int[] {1, 2, 3}, 4));     //1
+		System.out.println(subsetSum(new int[] {10, 20, 15}, 37)); //0
+		
+		System.out.println("Approach1");
+		System.out.println(subsetApproach1(new int[]{10,5,2,3,6},8,0));      //2
+		System.out.println(subsetApproach1(new int[] {10, 20, 15}, 0, 0));   //1
+		System.out.println(subsetApproach1(new int[] {1, 2, 3}, 4, 0));      //1
+		System.out.println(subsetApproach1(new int[] {10, 20, 15}, 37, 0));  //0 
 	}
 
 }
