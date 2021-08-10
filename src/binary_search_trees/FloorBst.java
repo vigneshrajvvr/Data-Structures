9b4fdf37c02f4d041e459703294fd636b49e41fc
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-08-10 20:56:54 +0530

    Floor of BST

---
+++ src/binary_search_trees/FloorBst.java
@@ -0,0 +1,31 @@
+package binary_search_trees;
+
+public class FloorBst {
+	
+	/*
+	 * To find floor of Binary Search Tree
+	 */
+
+	public Node floor(Node root, Node temp, int val) {
+		Node traversalNode = root;
+
+		while (traversalNode != null) {
+			if (traversalNode.getValue() == val) {
+				temp = root;
+				break;
+			} else if (val < traversalNode.getValue()) {
+				traversalNode = traversalNode.getLeft();
+			} else {
+				temp = traversalNode;
+				traversalNode = traversalNode.getRight();
+			}
+		}
+
+		return temp;
+	}
+
+	public static void main(String args[]) {
+
+	}
+
+}
