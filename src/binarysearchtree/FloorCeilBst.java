e7c7841b154121382f0c156bf461cd2cce6d109b
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-08-11 19:31:31 +0530

    Ceil of Bst

--- src/binarysearchtree/FloorCeilBst.java
+++ src/binarysearchtree/FloorCeilBst.java
@@ -1,6 +1,6 @@
 package binarysearchtree;
 
-public class FloorBst {
+public class FloorCeilBst {
 	
 	/*
 	 * To find floor of Binary Search Tree    
@@ -17,6 +17,28 @@ public class FloorBst {
 				traversalNode = traversalNode.getLeft();
 			} else {
 				temp = traversalNode;
+				traversalNode = traversalNode.getRight();
+			}
+		}
+
+		return temp;
+	}
+	
+	/*
+	 * To find ceil of Binary Search Tree    
+	 */
+
+	public Node ceil(Node root, Node temp, int val) {
+		Node traversalNode = root;
+
+		while (traversalNode != null) {
+			if (traversalNode.getValue() == val) {
+				temp = root;
+				break;
+			} else if (val < traversalNode.getValue()) {
+			    temp = traversalNode;	
+				traversalNode = traversalNode.getLeft();
+			} else {
 				traversalNode = traversalNode.getRight();
 			}
 		}
