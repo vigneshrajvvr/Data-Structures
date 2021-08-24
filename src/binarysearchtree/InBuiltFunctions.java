0576d544e995b4ef8afa2b7d36358063c32bc69e
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-08-24 20:20:29 +0530

    TreeMap inbuilt function

--- src/binarysearchtree/InBuiltFunctions.java
+++ src/binarysearchtree/InBuiltFunctions.java
@@ -1,12 +1,15 @@
 package binarysearchtree;
 
 import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class InBuiltFunctions {
 	
 	public static void main(String args[]) {
 		
+		System.out.println("Tree Set : ");
 		TreeSet<String> s = new TreeSet<>(); // Self balancing binary search tree
 		s.add("gfg");
 		s.add("courses");
@@ -37,6 +40,33 @@ public class InBuiltFunctions {
 		for(Integer x : treeSet) {
 			System.out.println(x);
 		}
+		
+		System.out.println("-------------------------------------------");
+		System.out.println("Tree Map : ");
+		TreeMap<Integer, String> t = new TreeMap<>(); // Stores the keys in stored order
+		t.put(10, "geeks");
+		t.put(15, "ide");
+		t.put(5, "courses");
+		
+		System.out.println(t);
+		System.out.println(t.containsKey(10));
+		
+		System.out.println(t.higherKey(10));
+		System.out.println(t.lowerKey(10));
+		System.out.println(t.floorKey(10));
+		System.out.println(t.ceilingKey(10));
+		
+		System.out.println(t.higherEntry(10).getValue());
+		System.out.println(t.lowerEntry(10).getValue());
+		System.out.println(t.floorEntry(10).getValue());
+		System.out.println(t.ceilingEntry(10).getValue());
+		
+		for(Map.Entry<Integer, String> e : t.entrySet()) {
+			System.out.println(" Key : " + e.getKey() + " Value : " + e.getValue());
+		}
+		
+		t.remove(10);
+		System.out.println("Size : " + t.size());
 		
 	}
 	
